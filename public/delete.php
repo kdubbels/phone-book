3e4347f2e6caaa081ebdb2fa176059cf573c941b
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2015-03-05 19:35:18 -0500

    Get on massively wrong track, get form to communicate with database, not yet Ajax

---
+++ public/delete.php
@@ -0,0 +1,5 @@
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/db_connection.php"); ?>
+<?php
+
+?>
--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php require_once("../includes/session.php"); ?>
 <?php require_once("../includes/db_connection.php"); ?>
-<?php include("../includes/layouts/header.php"); ?>
 <?php
     // Perform database query
     $query  = "SELECT * ";
@@ -12,7 +11,53 @@
         die("Database query failed.");
     }
 ?>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Phone Book</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery.tablesorter.js"></script>
+<script>
+function ajax_post(){
+    // Create our XMLHttpRequest object
+    var hr = new XMLHttpRequest();
+    // Create some variables we need to send to our PHP file
+    var url = "my_parse_file.php";
+    var fn = document.getElementById("first_name").value;
+    var ln = document.getElementById("last_name").value;
+    var ph = document.getElementById("phone").value;
+    var vars = "first_name="+fn+"&last_name="+ln+"&phone="+ph;
+    hr.open("POST", url, true);
+    // Set content type header information for sending url encoded variables in the request
+    hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    // Access the onreadystatechange event for the XMLHttpRequest object
+    hr.onreadystatechange = function() {
+        if(hr.readyState == 4 && hr.status == 200) {
+            var return_data = hr.responseText;
+            document.getElementById("status").innerHTML = return_data;
+        }
+    }
+    // Send the data to PHP now... and wait for response to update the status div
+    hr.send(vars); // Actually execute the request
+    document.getElementById("status").innerHTML = "processing...";
+}
+</script>
+<script>
+$(document).ready(function() 
+    { 
+        $("#tablelist").tablesorter(); 
+    } 
+); 
+</script>
+</head>
+<body>
+
+    <div class="container-fluid">
+    <h1>Phone Book</h1>
 
 
 
@@ -56,11 +101,11 @@ echo "<tr>";
                 echo "<td>" , $subject["phone"] , "</td>";
                 echo "<td style='text-align:center;'>";
   
-                    //we will use this links on next part of this post
-                    echo "<div class='editBtn customBtn'><button class='btn btn-default' type='submit'>Edit</button></div>";
+                    //edit button
+                    //echo "<div class='editBtn customBtn'><button class='btn btn-default' type='submit'>Edit</button></div>";
   
-                    //we will use this links on next part of this post
-                    echo "<div class='deleteBtn customBtn'><button class='btn btn-default' type='submit'><a href='#' onclick='return confirm('Are you sure?');'>Delete</a></button></div>";
+                    //delete button
+                    echo "<div class='deleteBtn customBtn'><button class='btn btn-default' type='submit'>Delete</button></div>";
                 echo "</td>";
             echo "</tr>";
         }
@@ -74,5 +119,6 @@ echo "<tr>";
 </div> <!-- Boot page container -->
 
 <script src="js/bootstrap.min.js"></script>
+
 </body>
 </html>
